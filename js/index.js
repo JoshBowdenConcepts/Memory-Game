0e57e8fba5bfa796597399675d30d7eb4c59973f
Josh Bowden <JBowden@simplifycompliance.com>
2018-07-05 21:10:01 -0400

    Added win validation and restart functionality

--- js/index.js
+++ js/index.js
@@ -32,6 +32,9 @@ var game = {
     var eseconds = end.getSeconds();
     console.log('End time: ' + ehours + ':' + eminutes + ':' + eseconds);
 
+    // Flip Cards Back
+    $('.cover').removeClass('lockedIn');
+    // Restart turns and stars
     this.start = false;
     console.log(this.start);
   },
@@ -40,49 +43,80 @@ var game = {
     this.moves += 1;
     console.log(this.moves);
     $('#turn').text(this.moves);
+    $('.win-moves').text(this.moves);
 
-    if(this.moves <= 6) {
+    if(this.moves <= 16) {
       // console.log('perfection');
       $('#stars').html("<i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i>");
-    } else if (this.moves <= 8) {
-      console.log('not perfection');
-      $('#stars').html("<i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star-half-alt'></i>");
-    } else if (this.moves <= 10) {
-      console.log('not perfection');
-      $('#stars').html("<i class='fas fa-star'></i><i class='fas fa-star'></i><i class='far fa-star'></i>");
-    } else if (this.moves <= 12) {
-      console.log('not perfection');
-      $('#stars').html("<i class='fas fa-star'></i><i class='fas fa-star-half-alt'></i><i class='far fa-star'></i>");
-    } else if (this.moves <= 14) {
-      console.log('not perfection');
-      $('#stars').html("<i class='fas fa-star'></i><i class='far fa-star'></i><i class='far fa-star'></i>");
-    } else if (this.moves <= 16) {
-      console.log('not perfection');
-      $('#stars').html("<i class='fas fa-star-half-alt'></i><i class='far fa-star'></i><i class='far fa-star'></i>");
+      $('.win-stars').text('3');
     } else if (this.moves <= 20) {
       console.log('not perfection');
+      $('#stars').html("<i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star-half-alt'></i>");
+      $('.win-stars').text('2.5');
+    } else if (this.moves <= 24) {
+      console.log('not perfection');
+      $('#stars').html("<i class='fas fa-star'></i><i class='fas fa-star'></i><i class='far fa-star'></i>");
+      $('.win-stars').text('2');
+    } else if (this.moves <= 28) {
+      console.log('not perfection');
+      $('#stars').html("<i class='fas fa-star'></i><i class='fas fa-star-half-alt'></i><i class='far fa-star'></i>");
+      $('.win-stars').text('1.5');
+    } else if (this.moves <= 32) {
+      console.log('not perfection');
+      $('#stars').html("<i class='fas fa-star'></i><i class='far fa-star'></i><i class='far fa-star'></i>");
+      $('.win-stars').text('1');
+    } else if (this.moves <= 36) {
+      console.log('not perfection');
+      $('#stars').html("<i class='fas fa-star-half-alt'></i><i class='far fa-star'></i><i class='far fa-star'></i>");
+      $('.win-stars').text('0.5');
+    } else if (this.moves <= 40) {
+      console.log('not perfection');
       $('#stars').html("<i class='far fa-star'></i><i class='far fa-star'></i><i class='far fa-star'></i>");
+      $('.win-stars').text('0');
     }
   },
   unflip: function() {
     if (game.flipCounter === 0) {
       game.waiting = false;
       if ($('.cover').hasClass('flip')) {
+        if(
+          $('.flip').eq(0).html()
+          ===
+          $('.flip').eq(1).html()
+        ) {
+          $('.flip').removeClass('flip').addClass('lockedIn');
+          console.log($('.lockedIn').length);
+          // On win show win screen
+          if($('.lockedIn').length === 16) {
+            $('footer').show();
+            game.end();
+          }
+        }
         $('.cover').removeClass('flip');
+        $('.cover').children('.hidden').hide();
       }
       // console.log('Waiting is: ' + game.waiting);
     }
   },
   restart: function() {
-    this.start = false;
-    this.flipCounter = 0;
-    this.moves = 0;
+    game.moves = -1;
+    game.move();
+    game.start = false;
+    game.flipCounter = 0;
     this.waiting = false;
-    location.reload();
+    $('.cover').removeClass('lockedIn');
   }
 }
 
 $(document).ready(function() {
+  $('footer').hide();
+  $('button').click(function() {
+    $('footer').hide();
+    game.moves = -1;
+    game.move();
+    game.start = false;
+  });
+
   $('#stars').html("<i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i>");
 
   // Start game - Take turn
@@ -92,23 +126,29 @@ $(document).ready(function() {
       if(!game.start) {
         game.setStart();
         $(this).addClass('flip');
+        // Show the item
+        $(this).children('.hidden').show();
         game.flipCounter += 1;
         // console.log('Flip counter: ' + game.flipCounter);
       } else {
         // If game has started and the div has been flipped
-        if ($(this).hasClass('flip')) {
+        if ($(this).hasClass('flip') || $(this).hasClass('lockedIn')) {
 
         } else if (game.flipCounter === 1) {
           game.waiting = true;
           game.move();
           $(this).addClass('flip');
+          // Show the item
+          $(this).children('.hidden').show();
           game.flipCounter = 0;
           // console.log('Flip counter: ' + game.flipCounter);
           setTimeout(game.unflip, 1000);
         } else {
-        // The card hasn't been flipped
+          // The card hasn't been flipped
           game.move();
           $(this).addClass('flip');
+          // Show the item
+          $(this).children('.hidden').show();
           game.flipCounter += 1;
           // console.log('Flip counter: ' + game.flipCounter);
         }
@@ -118,9 +158,7 @@ $(document).ready(function() {
 
   // Restart game
   $('#restart').click(function() {
-    if(game.start) {
-      game.end();
-    }
+    game.restart();
   });
 
 
